1c9e672f24f2ec53afdc1f2b507d3ac941163fef
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-11-01 13:08:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('*', function (req, res) {
 });
 // Create an http server and run it
 const server = http.createServer(app);
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 server.listen(port, function () {
   console.log('Express server running on *:' + port);
 });
